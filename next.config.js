762f0037aeb658d5b8d472b19082c0929c3b8994
CASCIO Joseph Hugo <cascio.hugo@gmail.com>
2023-07-14 18:38:50 +0200

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-	images: {
-		domains: ["cdn.myanimelist.net"],
-	},
+export const images = {
+	domains: ["cdn.myanimelist.net"],
 }
 
