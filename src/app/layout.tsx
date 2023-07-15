737f7a1dec2ff84b6a3591c543394a0165f6e3e1
CASCIO Joseph Hugo <cascio.hugo@gmail.com>
2023-07-15 15:50:32 +0200

    fix: test icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
 		{ media: "(prefers-color-scheme: dark)", color: "black" },
 	],
 	icons: {
-		icon: "/logo.png",
-		shortcut: "/logo.png",
+		icon: "/favicon.ico",
+		shortcut: "/favicon-16x16.png",
 		apple: "/apple-touch-icon.png",
 	},
 }
