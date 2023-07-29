e5fe88b563943fb0b0480cfb5f3ad408d8682677
CASCIO Joseph Hugo <cascio.hugo@gmail.com>
2023-07-29 16:44:21 +0200

    fix: test icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,19 +32,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/apple-touch-icon.png"
+					href="apple-touch-icon.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="/favicon-32x32.png"
+					href="favicon-32x32.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="/favicon-16x16.png"
+					href="favicon-16x16.png"
 				/>
 				<link rel="manifest" href="/site.webmanifest" />
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
